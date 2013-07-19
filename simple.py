d8d10d14362f0765d0760a94beffb1cacf1a8b6d
kkoolwal <kushalkoolwal@gmail.com>
2013-07-18 21:16:37 -0700

    Adding global variables.

--- simple.py
+++ simple.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 
+# Python notes based on Writing Idiomatic Python by Jeff Knupp
+# Use Python Package Index (PyPI)
+# Use pip install <name-of-package>
+# Use itertools module
+# Use os.path module
+
+# global variables should be in CAPS
+SECONDS_IN_A_DAY = 60 * 60 * 24
 
 def myfunc(var):
 
 	print var
 
+
 str = 'Hello World'
 myfunc(str)
+
+print SECONDS_IN_A_DAY
