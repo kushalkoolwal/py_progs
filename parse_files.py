db832fe6074f7797bf66edfb16d439ab5c490be7
kkoolwal <kushalkoolwal@gmail.com>
2013-07-18 23:39:37 -0700

    open and read file

--- parse_files.py
+++ parse_files.py
@@ -4,7 +4,23 @@ from __future__ import print_function
 import sys
 
 def main():
-	print ('Hello')
+
+	
+
+	# open the file
+	file = open('syslog')
+
+	#reads only one line
+	log_data = file.readline()
+
+	# reads entire file
+	log_data = file.readlines()
+
+
+	print(log_data)
+
+	# close the file
+	file.close
 
 if __name__ == '__main__':
 	sys.exit(main())
