6390f41d0866b2d437d9b2db72872802b6f89a01
kkoolwal <kushalkoolwal@gmail.com>
2013-07-19 07:33:27 -0700

    Added re.compile.

--- parse_files.py
+++ parse_files.py
@@ -25,13 +25,25 @@ def main():
 	#for line in open('syslog', 'r'):
 	#	print (line)
 
+
+	# 'r' stands for raw and is simply an indicator to Python that no string
+	# interpolation should be performed on the string
+
 	# another way to read file line-by-line
 	pattern = 'Jul 16' # use re.match for beginning of line only
 	#pattern = 'CRON' # use re.search to match anywhere
 	with open('syslog', 'r') as f:
 		for line in f:
-			if re.match(pattern, line):
+			match = re.match(pattern, line)
+			if match:
 				print (line)
+
+	pattern = re.compile("d")
+	pattern.search("dog")
+	if pattern.search:
+		print ('Match found')
+
+	
 
 if __name__ == '__main__':
 	sys.exit(main())
