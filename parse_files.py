a1713437fad8ef7d79fda02e7b67057aa58689d2
kkoolwal <kushalkoolwal@gmail.com>
2013-07-19 00:10:56 -0700

    Added pattern match.

--- parse_files.py
+++ parse_files.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from __future__ import print_function
 import sys
+import re
 
 def main():
 
@@ -13,19 +14,23 @@ def main():
 	# reads entire file into memory
 	# not good for large files
 	log_data = file.readlines()
-
 	#log_data = file..readlines()[4:] # skips first 4 lines
 
 	# close the file
 	file.close
 
-	print(log_data)
+	#print(log_data)
 
 	# read file line-by-line
-	for line in open('syslog', 'r'):
-		print (line)
-	
+	#for line in open('syslog', 'r'):
+	#	print (line)
 
+	# another way to read file line-by-line
+	pattern = '^Jul 16'
+	with open('syslog', 'r') as f:
+		for line in f:
+			if re.match(pattern, line):
+				print (line)
 
 if __name__ == '__main__':
 	sys.exit(main())
