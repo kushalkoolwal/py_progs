15d640d5c677e303b699b894e3de2c10b583b6e1
kkoolwal <kushalkoolwal@gmail.com>
2013-07-18 21:37:40 -0700

    Added return statement.

--- simple.py
+++ simple.py
@@ -6,6 +6,8 @@
 # Use itertools module
 # Use os.path module
 
+import sys
+
 # global variables should be in CAPS (3.1.1)
 SECONDS_IN_A_DAY = 60 * 60 * 24
 
@@ -15,15 +17,25 @@ def myfunc(var):
 	print var
 
 
+# always create main function for your program (3.5.2)
+def main():
+	str = 'Hello World'
+	myfunc(str)
 
+	print SECONDS_IN_A_DAY
+
+	# Success = 0; Error = Other
+	sys.exit(0)
+
+	# return below is optional if you have sys.ext(0), as default return value
+	# in python is 'None' which sys.exit treats as 'exit with 0'.
+	return 0
+	
 # Use the if __name__ == '__main__' pattern to allow a file to be both
 # imported and run directly (3.5.1)
 
 # Will only run if script is executed directly,
 # not when the file is imported as a module
 if __name__ == '__main__':
-	str = 'Hello World'
-	myfunc(str)
-
-	print SECONDS_IN_A_DAY
-	
+# call sys.exit with return value of main function as the parameter (3.5.2).
+	sys.exit(main())
